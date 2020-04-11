606395d89f8b4c6ba450373b09f29a04507c3bc1
Ramkumar <ramkumars.mailme@gmail.com>
2020-04-11 10:22:28 -0400

    test

--- src/main/java/io/github/jhipster/sample/ApplicationWebXml.java
+++ src/main/java/io/github/jhipster/sample/ApplicationWebXml.java
@@ -7,6 +7,7 @@ import io.github.jhipster.config.DefaultProfileUtil;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
 /**
  * This is a helper Java class that provides an alternative to creating a {@code web.xml}.
  * This will be invoked only when the application is deployed to a Servlet container like Tomcat, JBoss etc.
